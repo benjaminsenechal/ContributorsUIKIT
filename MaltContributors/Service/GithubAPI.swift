5638429caa5c84769d21946c1a187d4ffd919062
benjaminsenechal <senechal.benjamin1@gmail.com>
2022-12-07 09:11:47 +0100

    chore: add secret

--- MaltContributors/Service/GithubAPI.swift
+++ MaltContributors/Service/GithubAPI.swift
@@ -11,5 +11,5 @@ let kBaseURL = "https://api.github.com/"
 
 enum GithubAPI {
     static let base = URL(string: kBaseURL)!
-    static let token = "Bearer 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"
+    static let token = Bundle.main.infoDictionary?["TOKEN_GITHUB"] as? String
 }
